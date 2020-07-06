d0c5c526237a3c9353dc23b3f200980cd69ad1a2
Egor Bedov <bedovegor2203@gmail.com>
2020-07-06 17:55:58 +0300

    Fix left/right movements of rectangles

--- src/core/structure/basicObject.js
+++ src/core/structure/basicObject.js
@@ -16,6 +16,7 @@ class BasicObject {
     cantMoveLeft(table) {}
 
     moveLeft(table) {
+        this.place = this.place.sort((prev, curr) => prev.column - curr.column);
         this.place = this.place.map(coord => {
             table[coord.row][coord.column] = 0;
             coord.column--;
@@ -27,10 +28,12 @@ class BasicObject {
     cantMoveRight() {}
 
     moveRight(table) {
+        this.place = this.place.sort((prev, curr) => curr.column - prev.column);
         this.place = this.place.map(coord => {
             table[coord.row][coord.column] = 0;
             coord.column++;
             table[coord.row][coord.column] = this.index;
+            console.log(table);
             return coord;
         });
     }
